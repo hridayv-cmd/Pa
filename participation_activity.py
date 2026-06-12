f7118b431bbf3888111781b440ea4a1fe209b082
Hriday <hridayvermani12@gmail.com>
2026-06-12 09:12:09 -0400

    Add sandwich processing loop to track completed orders

--- participation_activity.py
+++ participation_activity.py
@@ -1,4 +1,13 @@
 sandwich_orders = ["tuna", "ham", "turkey", "veggie", "roast beef"]
 finished_sandwiches = []
 
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop(0)
+    print(f"I made your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+
+print("\nThe following sandwiches were made:")
+for sandwich in finished_sandwiches:
+    print(f"- {sandwich}")
 
